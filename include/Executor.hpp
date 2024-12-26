f57fe311b7eb471a9df38b62562f93867d4032c0
weixianghan <uestcwxh@qq.com>
2024-12-26 23:22:04 +0900

    feat: Add test cases for initial and default poses- should_return_init_pose_when_without_command: Ensures the initial pose is returned in the absence of a command.- should_return_default_pose_when_without_init_and_command: Ensures the default pose is returned when there is no initial pose or command.

--- include/Executor.hpp
+++ include/Executor.hpp
@@ -12,16 +12,16 @@ namespace adas
     {
     public:
         // Caller should delete *executor when it is no longer needed.
-        static Executor *NewExecutor(const Pose &pose) noexcept;
+        static Executor *NewExecutor(const Pose &pose = {0, 0, 'N'}) noexcept;
 
-        Executor(void) = default;               // 使用编译器生成的默认实现
-        virtual ~Executor(void) = default;      
-        Executor(const Executor &) = delete;    // 禁止拷贝和赋值
+        Executor(void) = default; // 使用编译器生成的默认实现
+        virtual ~Executor(void) = default;
+        Executor(const Executor &) = delete; // 禁止拷贝和赋值
         Executor &operator=(const Executor &) = delete;
 
         // 执行指令
-        virtual void Execute(const std::string &command) noexcept = 0; 
+        virtual void Execute(const std::string &command) noexcept = 0;
         // 查询当前位置信息
-        virtual Pose Query(void) const noexcept = 0; 
+        virtual Pose Query(void) const noexcept = 0;
     };
 } // namespace adas
--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -9,13 +9,22 @@ namespace adas
         // 为Pose定义==运算符重载，使得在测试中比较Pose对象变得简单明了，std::tie提高代码可读性
         return std::tie(lhs.x, lhs.y, lhs.heading) == std::tie(rhs.x, rhs.y, rhs.heading);
     }
-    // 格式：TEST(测试套件名，用例名) 遵循Arrange - Act - Assert(AAA) 模式
+    //gtest格式：TEST(测试套件名，用例名) 遵循Arrange - Act - Assert(AAA) 模式
     TEST(ExecutorTest, should_return_init_pose_when_without_command)
     {
         // Arrange: 
         std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
         const Pose target({0, 0, 'E'});
         // Assert:
+        ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_default_pose_when_without_init_and_command)
+    {
+        // given
+        std::unique_ptr<Executor> executor(Executor::NewExecutor());
+        // when
+        // then
+        const Pose target({0, 0, 'N'});
         ASSERT_EQ(target, executor->Query());
     }
 } // namespace adas
