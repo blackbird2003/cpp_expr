46b47c41bf2b35e655315997d64b3256cd949918
weixianghan <uestcwxh@qq.com>
2024-12-28 06:26:39 +0900

    Expr3 实现函数式编程，删除ICommand，修改指令和Execute()表驱动

--- src/Command.hpp
+++ src/Command.hpp
@@ -5,66 +5,96 @@
 
 // Expr2 解耦循环依赖：executor -> poseHandler
 
+// Expr3 改为函数式编程
+#include <functional>
+
 namespace adas
 {
-
-    class ICommand
-    {
-    public:
-        virtual ~ICommand() = default;
-        virtual void DoOperate(PoseHandler &poseHandler) const noexcept = 0;
-    };
+    // class ICommand
+    // {
+    // public:
+    //     virtual ~ICommand() = default;
+    //     virtual void DoOperate(PoseHandler &poseHandler) const noexcept = 0;
+    // };
 
     // Expr2 Move/TurnLeft/TurnRight封装到类中
     // 移动
-    class MoveCommand final : public ICommand
+    class MoveCommand final //: public ICommand
     {
     public:
-        void DoOperate(PoseHandler &poseHandler) const noexcept override
+        // void DoOperate(PoseHandler &poseHandler) const noexcept override
+        // {
+        //     // 如果处于加速状态，则移动两次
+        //     if (poseHandler.IsFast())
+        //     {
+        //         poseHandler.Move();
+        //     }
+        //     poseHandler.Move(); // 默认移动一次
+        // }
+        const std::function<void(PoseHandler &poseHandler)> operate = [](PoseHandler &poseHandler) noexcept
         {
-            // 如果处于加速状态，则移动两次
             if (poseHandler.IsFast())
             {
                 poseHandler.Move();
             }
-            poseHandler.Move(); // 默认移动一次
-        }
+            poseHandler.Move();
+        };
     };
     // 左转
-    class TurnLeftCommand final : public ICommand
+    class TurnLeftCommand final //: public ICommand
     {
     public:
-        void DoOperate(PoseHandler &poseHandler) const noexcept override
+        // void DoOperate(PoseHandler &poseHandler) const noexcept override
+        // {
+        //     // 如果处于加速状态，先前进一步
+        //     if (poseHandler.IsFast())
+        //     {
+        //         poseHandler.Move();
+        //     }
+        //     poseHandler.TurnLeft(); // 执行左转
+        // }
+        const std::function<void(PoseHandler &poseHandler)> operate = [](PoseHandler &poseHandler) noexcept
         {
-            // 如果处于加速状态，先前进一步
             if (poseHandler.IsFast())
             {
                 poseHandler.Move();
             }
-            poseHandler.TurnLeft(); // 执行左转
-        }
+            poseHandler.TurnLeft();
+        };
     };
     // 右转
-    class TurnRightCommand final : public ICommand
+    class TurnRightCommand final //: public ICommand
     {
     public:
-        void DoOperate(PoseHandler &poseHandler) const noexcept override
+        // void DoOperate(PoseHandler &poseHandler) const noexcept override
+        // {
+        //     // 如果处于加速状态，先前进一步
+        //     if (poseHandler.IsFast())
+        //     {
+        //         poseHandler.Move();
+        //     }
+        //     poseHandler.TurnRight(); // 执行右转
+        // }
+        const std::function<void(PoseHandler &poseHandler)> operate = [](PoseHandler &poseHandler) noexcept
         {
-            // 如果处于加速状态，先前进一步
             if (poseHandler.IsFast())
             {
                 poseHandler.Move();
             }
-            poseHandler.TurnRight(); // 执行右转
-        }
+            poseHandler.TurnRight();
+        };
     };
     // Expr 2 Fast Command类
-    class FastCommand final : public ICommand
+    class FastCommand final //: public ICommand
     {
     public:
-        void DoOperate(PoseHandler &poseHandler) const noexcept override
+        // void DoOperate(PoseHandler &poseHandler) const noexcept override
+        // {
+        //     poseHandler.Fast(); // 切换加速状态
+        // }
+        const std::function<void(PoseHandler &poseHandler)> operate = [](PoseHandler &poseHandler) noexcept
         {
-            poseHandler.Fast(); // 切换加速状态
-        }
+            poseHandler.Fast();
+        };
     };
 } // namespace adas
